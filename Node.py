0479e85f9b7471c5f841f212594fce877cdd062b
RashmiICT <96565@fhss.sjp.ac.lk>
2021-05-11 11:11:22 +0530

    new comment

--- Node.py
+++ Node.py
@@ -9,17 +9,17 @@ class Node:  # Create a class named as Node
     def setData(self, data):  # insert or set the instance values or data
         self.data = data
 
-    def getData(self):  # get the data
+    def getData(self):  # get the data of the node
         return self.data
 
-    def setNext(self, address):  # set next data
+    def setNext(self, address):  # set next data  of the node
         self.nref = address
 
-    def getNext(self):  # get next data
+    def getNext(self):  # get next data  of the node
         return self.nref
 
-    def setPrevious(self, address):  # set previous address
+    def setPrevious(self, address):  # set previous address  of the node
         self.pref = address
 
-    def getPrevious(self):  # get previous data
+    def getPrevious(self):  # get previous data  of the node
         return self.pref
