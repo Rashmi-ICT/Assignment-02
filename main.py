f74c760cdbd277392d2ec6341e342cb0a7201022
RashmiICT <96565@fhss.sjp.ac.lk>
2021-05-13 20:45:17 +0530

    ADD COMMENT

--- main.py
+++ main.py
@@ -1,37 +1,42 @@
 from DoublyLinkedList import DoublyLinkedList
 
-d = DoublyLinkedList()
-'''d.add_begin(89)
-d.add_begin(90)
-d.add_begin(00)'''
+Doubly = DoublyLinkedList()
 
-d.add_begin(90)
-d.add_begin(00)
-d.add_end(2222)
-d.add_end(567)
+Doubly.add_begin(100)
+Doubly.add_begin(200)
+Doubly.add_begin(300)
 
+Doubly.add_end(400)
+Doubly.add_end(500)
+Doubly.add_end(600)
+
+Doubly.add_nodePosition(-1, 000)
+Doubly.add_nodePosition(0, 111)
+Doubly.add_nodePosition(1, 222)
+Doubly.add_nodePosition(2, 333)
+Doubly.add_nodePosition(3, 444)
+Doubly.add_nodePosition(4, 555)
+Doubly.add_nodePosition(5, 666)
+Doubly.add_nodePosition(11, 777)
+
+Doubly.add_end(56658)
+Doubly.print_LL_reverse()
+
+x = Doubly.Doubly_Length
+print(x)
+
+Doubly.add_nodePosition(0, 8888)
+Doubly.print_LL_reverse()
+
+Doubly.remove_begin()
+Doubly.remove_end()
+
+Doubly.remove_nodePosition(12)
+
+Doubly.print_LL_reverse()
+
+print()
+# print("searching node prev",Doubly.searchPrev(5),"\n","searching node "Doubly.searchNode(5),"\n","searching node next",Doubly.searchNext(5))
+Doubly.print_LL_reverse()
 print("\n")
-x = d.Doubly_Length()
-print("length",x)
-print("\n")
-
-d.add_nodePosition(6,34)
-
-'''d.remove_begin()
-d.remove_end()'''
-d.print_LL()
-d.print_LL_reverse()
-d.remove_nodePosition(3)
-
-d.print_LL()
-d.print_LL_reverse()
-
-print(d.len)
-print("\n")
-
-'''print(d.search_given(3))'''
-
-print("\n")
-x = d.Doubly_Length()
-print("length",x)
-
+Doubly.print_LL()
